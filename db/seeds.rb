3da5d1ada030eaaa1299ce6e6e8cdb916851250e
Mario Avila <avilamario16@gmail.com>
2015-11-08 12:06:11 -0600

    Added forgoten seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CategoryPost.create(id: 1, name: "Buscar")
+CategoryPost.create(id: 2, name: "Ofrezco")
+Category.create(id: 1, name: "Servicio")
+Category.create(id: 2, name: "Articulo")
+School.create(id: 1, name: "FIME")
+School.create(id: 2, name: "FACPYA")
+School.create(id: 3, name: "FOD")
+School.create(id: 4, name: "FARQ")
