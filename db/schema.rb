f1a77fac43cf5dc2194b4ee6b06b9d5f6a87489d
LuisSFU <Saloch_1s@hotmail.com>
2015-11-07 18:40:10 -0600

    Added CategoryPost Controller, modified Schools Controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107213558) do
+ActiveRecord::Schema.define(version: 20151108002313) do
+
+  create_table "category_posts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
