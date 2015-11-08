228f1dd4c7b99d9d6667a71a98fe5b4c08a8d7df
Mario Avila <avilamario16@gmail.com>
2015-11-07 19:55:14 -0600

    Created relationships for places with everything else

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,37 @@
 module PostsHelper
+  def select_category
+
+    category = Category.all
+      final_categories = []
+
+      category.each do |c|
+        final_categories << [c.name, c.id]
+
+      end
+      final_categories
+  end
+
+  def select_school
+
+    school = School.all
+      final_schools = []
+
+      school.each do |c|
+        final_schools << [c.name, c.id]
+
+      end
+      final_schools
+  end
+
+  def select_category_post
+
+    category_post = CategoryPost.all
+      final_category_posts = []
+
+      category_post.each do |c|
+        final_category_posts << [c.name, c.id]
+
+      end
+      final_category_posts
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :school
+  belongs_to :category_post
+  belongs_to :user
 end
