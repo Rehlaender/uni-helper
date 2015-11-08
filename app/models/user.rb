7df27da3bd5ee2ac59ee49d4c32a57478069f6cd
Jorge Garcia <hegg777@gmail.com>
2015-11-07 21:25:04 -0600

    User password digest and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3}
+
+  validates :user, presence: true, length: { minimum: 3}, uniqueness: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :mail, presence: true, length: { maximum: 200},
+            format: { with: VALID_EMAIL_REGEX },uniqueness:{ case_sensitive: false }
+
+  validates :telephone, presence: true, length: {minimum: 6}
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108020908) do
+ActiveRecord::Schema.define(version: 20151108031031) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -59,8 +59,9 @@ ActiveRecord::Schema.define(version: 20151108020908) do
     t.string   "user"
     t.string   "mail"
     t.string   "telephone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
