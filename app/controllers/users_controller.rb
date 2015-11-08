326eaa4ea6757cec6ea77fcfd2500a3b4d1a43b1
Jorge Garcia <hegg777@gmail.com>
2015-11-07 23:42:29 -0600

    Users sessions...working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+
   def index
     @users = User.all
   end
@@ -15,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-    
+
      redirect_to @user
     else
      render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+
   resources :categories
   resources :users
   resources :posts
